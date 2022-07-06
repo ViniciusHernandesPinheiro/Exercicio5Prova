449d136a92942f8eb260c86af40aa7e1d938d211
vinic <vinic@PC-VINI>
2022-07-06 16:41:34 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio5Prova/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5Prova
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //RA: 6322036
+            //Nome: Vinícius Hernandes Pinheiro
+
+            Console.WriteLine("Exercicio 5");
+
+            Console.WriteLine("Numeros pares entre 1 e 100");
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
